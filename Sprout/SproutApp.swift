c9721665d5ff631d3391db73649233a39afcd0cd
Pierluigi De Meo <pierludm2002@gmail.com>
2025-10-16 12:19:34 +0200

    Initial Commit

---
+++ Sprout/SproutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SproutApp.swift
+//  Sprout
+//
+//  Created by Pierluigi De Meo on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SproutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
