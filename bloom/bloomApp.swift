59c26ccfc3a78510912d01f1e08455ecf39a1950
Pourya <pourmaz@yahoo.com>
2025-10-20 12:34:49 +0200

    test commit

---
+++ bloom/bloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  bloomApp.swift
+//  bloom
+//
+//  Created by Pourya Mazinani on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct bloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
