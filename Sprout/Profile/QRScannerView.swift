75b207139d560d8168c570cc22c34cae0b6d2012
nori;;code <persicoeleonora150@gmail.com>
2025-10-24 12:36:43 +0200

    QR Code View

---
+++ Sprout/Profile/QRScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  QRScannerView.swift
+//  Sprout
+//
+//  Created by Eleonora Persico on 24/10/25.
+//
+
+import SwiftUI
+
+struct QRScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QRScannerView()
+}
---
+++ Sprout/Profile/ScanButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanButtonView.swift
+//  Sprout
+//
+//  Created by Eleonora Persico on 24/10/25.
+//
+
+import SwiftUI
+
+struct ScanButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanButtonView()
+}
